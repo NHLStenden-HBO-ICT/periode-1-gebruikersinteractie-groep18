c476b2713072f038624d93d9551b7eab616c293a
robinbouma7 <robinbouma711@gmail.com>
2023-10-17 14:36:29 +0200

    generating memory cards grid

--- MemoryBoard.xaml.cs
+++ MemoryBoard.xaml.cs
@@ -23,10 +23,24 @@ namespace MemoryGame {
     public partial class MemoryBoard : Page {
         public MemoryBoard() {
             InitializeComponent();
+            parent = parentGrid;
+            
         }
+
+        public static int width;
+        public static int height;
+
+
         public static List<List<int>> cards = new List<List<int>>();
+
+        public static Grid cardGrid;
+        private static Grid parent;
+
         public static bool generate(int w, int h) {
-            int cardnum = w * h;
+            width = w;
+            height = h;
+
+            int cardnum = width * height;
             if(cardnum % 2 == 0) { 
                 //hoeveelheid kaarten is een even getal
                 List<int> sortedcards = new List<int>();
@@ -37,10 +51,10 @@ namespace MemoryGame {
                 }
                 Random rnd = new Random();
                 Trace.WriteLine("memory layout:");
-                for (int i = 0; i < h; i++) {
+                for (int i = 0; i < height; i++) {
                     //genereer een tijdelijke lijst om toe te kunnen voegen aan de cards lijst
                     List<int> templist = new List<int>();
-                    for(int j = 0; j < w; j++) {
+                    for(int j = 0; j < width; j++) {
                         //genereer een willekeurig nummer
                         int rand = rnd.Next(0, sortedcards.Count());
                         //voeg de id uit de lijst toe aan de tijdelijke lijst en verwijder heb uit de gesorteerde lijst
@@ -58,6 +72,45 @@ namespace MemoryGame {
                 Trace.WriteLine("de ingevoegde grootte levert een oneven aantal kaarten.");
                 return false;
             }
+
+            // Maakt een nieuw grid aan
+            cardGrid = new Grid();
+            cardGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            cardGrid.VerticalAlignment = VerticalAlignment.Center;
+
+            //voeg columns toe
+            for(int i = 0; i < width; i++) {
+                ColumnDefinition tempcol = new ColumnDefinition();
+                cardGrid.ColumnDefinitions.Add(tempcol);
+            }
+            //voeg rows toe
+            for(int i = 0; i < height; i++) {
+                RowDefinition temprow = new RowDefinition();
+                cardGrid.RowDefinitions.Add(temprow);
+            }
+
+            for(int i = 0; i < height; i++) {
+                for(int j = 0; j < width; j++) {
+                    Border rectangle = new() {
+                        Background = new SolidColorBrush(Color.FromRgb(255, 102, 102)),
+                        BorderBrush = new SolidColorBrush(Color.FromRgb(43, 43, 43)),
+                        BorderThickness = new Thickness(4),
+                        CornerRadius = new CornerRadius(12),
+                        // De ruimte tussen kaarten
+                        Margin = new Thickness(5),
+                        Height = 100,
+                        Width = 100,
+                    };
+                    //zet de row on column
+                    Grid.SetColumn(rectangle, j);
+                    Grid.SetRow(rectangle, i);
+                    //voeg hem toe aan de card grid
+                    cardGrid.Children.Add(rectangle);
+                }
+            }
+            //voeg de card grid toe aan de parent grid
+            parent.Children.Add(cardGrid);
+
             return true;
         }
     }
