a19e821cc54c561452ba75f4202ac52c762a48d2
SjamPam <Jimbrandinga@gmail.com>
2023-10-18 09:55:26 +0200

    Thema keuze scherm, Alles van Sil gestolen.

---
+++ Thema keuze scherm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for Thema_keuze_scherm.xaml
+    /// </summary>
+    public partial class Thema_keuze_scherm : Page
+    {
+        public Thema_keuze_scherm()
+        {
+            InitializeComponent();
+        }
+    }
+}
